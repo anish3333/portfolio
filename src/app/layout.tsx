51da68c03281bdbd6c8ab7fbfd71ee2a142ab067
Anish Awasthi <anishawasthi16@gmail.com>
2024-09-09 00:13:20 +0530

    issues related to integration resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script"; // Import Script
+import ChatBot from "@/components/ChatBot";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,15 +10,6 @@ export const metadata: Metadata = {
   title: "Anish Awasthi",
   description: "portfolio",
 };
-
-// Inline declaration of Voiceflow types
-interface VoiceflowChat {
-  load: (config: {
-    verify: { projectID: string };
-    url: string;
-    versionID: string;
-  }) => void;
-}
 
 export default function RootLayout({
   children,
@@ -28,21 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#1A2130]`}>
-        {/* Using the Next.js Script component */}
-        <Script
-          id="voiceflow-widget"
-          src="https://cdn.voiceflow.com/widget/bundle.mjs"
-          onLoad={() => {
-            const vfWindow = window as typeof window & {
-              voiceflow?: { chat: VoiceflowChat };
-            };
-            vfWindow.voiceflow?.chat.load({
-              verify: { projectID: "66ce337b23b6c24cc9d62f24" },
-              url: "https://general-runtime.voiceflow.com",
-              versionID: "production",
-            });
-          }}
-        />
+        {/* Use ClientScript for voiceflow */}
+        <ChatBot />
         {children}
         <Toaster
           position="bottom-left"
